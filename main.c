32268c0bed0001fe0bdc516834a2d3799e53bfbc
crystlee1005 <crystlee1005@sookmyung.ac.kr>
2020-10-08 15:45:41 +0900

    실습2

--- main.c
+++ main.c
@@ -4,19 +4,27 @@
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
    #include <stdio.h> 
-
+void sumTwo(int a, int b)
+{
+	   printf("%i", a+b);
+}
+void square(int n)
+{
+	   printf("%i", n*n);
+}
+void get_max(int x, int y)
+{
+	   if ( x>y)
+	   printf("%i", x);
+	   else if ( x<y)
+	   printf("%i", y); 
+}
 int main(void)
 {
-	int i;
-	
-	for( i = 0; i< 10; i++)
-	printf("*");
-	
-	for( i = 0; i< 10; i++)
-	printf("*");
-	
-	for( i = 0; i< 10; i++)
-	printf("*");
+	sumTwo( 1, 2);
+	square(3);
+	get_max(4,5);
+
 	
 	return 0;
 }
